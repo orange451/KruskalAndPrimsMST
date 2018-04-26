0b4d1d98041ecec37ad1fa9a2f8d3ea7b5fbaf2b
orange451 <drew.hamilton@mac.com>
2018-04-25 23:48:43 -0400

    Update

--- src/algo/degreeMST/data/Subset.java
+++ src/algo/degreeMST/data/Subset.java
@@ -1,5 +1,11 @@
 package algo.degreeMST.data;
+
 public class Subset {
 	public int parent;
 	public int rank;
+	
+	public Subset(int parent, int rank) {
+		this.parent = parent;
+		this.rank = rank;
+	}
 };
